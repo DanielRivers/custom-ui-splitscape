892f003ecd86e1945ded86fe9c7f15a18835f921
Daniel Rivers <me@danielrivers.com>
2026-06-18 11:21:01 +0000

    switch to sign up

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -26,7 +26,7 @@ const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
           data-kinde-change-connection-id={"conn_01928e4c2a8fc011b8b21ce728e5e4ac"}
           data-kinde-change-connection-psid={context.session.pipelineStepId}
           data-kinde-change-connection-action={JSON.stringify(context.actions.switchConnection)}
-          data-kinde-change-connection-auth-intent="sign_in"
+          data-kinde-change-connection-auth-intent="sign_up"
           data-kinde-change-connection-login-hint={context.auth?.providedEmail}
         >
           Sign in with Google
