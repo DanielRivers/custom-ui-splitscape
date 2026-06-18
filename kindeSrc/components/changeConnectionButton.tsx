98a8e9a48856591e0fa5dbb6369a1975b9851693
Daniel Rivers <me@danielrivers.com>
2026-06-18 13:14:53 +0000

    make component

---
+++ kindeSrc/components/changeConnectionButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export enum AuthIntent {
+  Register = "sign_up",
+  Login = "sign_in",
+}
+
+interface ChangeConnectionButtonProps {
+  connectionId: string;
+  pipelineStepId: string;
+  switchConnectionAction: unknown;
+  authIntent?: AuthIntent;
+  loginHint?: string;
+  children: React.ReactNode;
+}
+
+export const ChangeConnectionButton: React.FC<ChangeConnectionButtonProps> = ({
+  connectionId,
+  pipelineStepId,
+  switchConnectionAction,
+  authIntent = AuthIntent.Register,
+  loginHint,
+  children,
+}) => {
+  return (
+    <button
+      type="button"
+      data-kinde-change-connection-button="true"
+      data-kinde-change-connection-id={connectionId}
+      data-kinde-change-connection-psid={pipelineStepId}
+      data-kinde-change-connection-action={JSON.stringify(switchConnectionAction)}
+      data-kinde-change-connection-auth-intent={authIntent}
+      data-kinde-change-connection-login-hint={loginHint}
+    >
+      {children}
+    </button>
+  );
+};
--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { Widget } from "../../../../components/widget";
+import { ChangeConnectionButton, AuthIntent } from "../../../../components/changeConnectionButton";
 import { DefaultLayout } from "../../../../layouts/default";
 import { Root } from "../../../../root";
 import { type KindePageEvent } from "@kinde/infrastructure";
@@ -20,17 +21,15 @@ const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
           description={context.widget.content.description}
           onButtonClick={handleButtonClick}
         />
-        <button
-          type="button"
-          data-kinde-change-connection-button="true"
-          data-kinde-change-connection-id={"conn_01928e4c2a8fc011b8b21ce728e5e4ac"}
-          data-kinde-change-connection-psid={context.session.pipelineStepId}
-          data-kinde-change-connection-action={JSON.stringify(context.actions.switchConnection)}
-          data-kinde-change-connection-auth-intent="sign_up"
-          data-kinde-change-connection-login-hint={context.auth?.providedEmail}
+        <ChangeConnectionButton
+          connectionId="conn_01928e4c2a8fc011b8b21ce728e5e4ac"
+          pipelineStepId={context.session.pipelineStepId}
+          switchConnectionAction={context.actions.switchConnection}
+          authIntent={AuthIntent.Register}
+          loginHint={context.auth?.providedEmail}
         >
           Sign in with Google
-        </button>
+        </ChangeConnectionButton>
       </DefaultLayout>
     </Root>
   );
